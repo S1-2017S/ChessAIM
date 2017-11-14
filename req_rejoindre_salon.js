75141d7594c7804b15f2332c502fb59a4be131a3
BENNACEUR <BENNACEUR@intechS1.fr>
2017-11-14 15:52:54 +0100

    Création accès salon

---
+++ req_rejoindre_salon.js
@@ -0,0 +1,34 @@
+//==========================================================================
+// Rejoindre le salon de jeu
+// Bennaceur, Fumey-Humbert, Mercier-Handyside, Vasseur
+// Version : 14/11/2017
+//==========================================================================
+
+"use strict";
+
+var fs = require('fs');
+var path = require('path');
+
+var req_rejoindre_salon = function(req,res,query) {
+
+	var marqueurs;
+	var page;
+	
+	page = fs.readFileSync('res_salon.html', 'UTF-8');
+
+
+	marqueurs = {};
+	marqueurs.login = query.login;
+	marqueurs.date = query.date;
+	marqueurs.astuce = query.astuce;
+
+
+	page = page.supplant(marqueurs);
+
+	res.writeHead(200,{'Content-type': 'text/html'});
+	res.write(page);
+	res.end();
+
+};
+
+module.exports = req_rejoindre_salon;
