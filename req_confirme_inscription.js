7bd8159e03bf46dce753480e3ced4dd63aa21c59
BENNACEUR <BENNACEUR@intechS1.fr>
2017-11-20 14:49:17 +0100

    PUSH SID PUSH

--- req_confirme_inscription.js
+++ req_confirme_inscription.js
@@ -23,6 +23,7 @@ var trait = function (req, res, query) {
 	var pseudo_len;
 	var pseudo_check;
 	var pwd_check;
+	var pwd_check2;
 	var i;
 	// ON LIT LES COMPTES EXISTANTS
 
@@ -63,7 +64,7 @@ var trait = function (req, res, query) {
 			if (query.pseudo[i] === code[j]) {
 				compteur++;
 			}
-		 } console.log(compteur);
+		} console.log(compteur);
 	}
 	if (compteur === query.pseudo.length) {
 		pseudo_check = true;
@@ -77,97 +78,113 @@ var trait = function (req, res, query) {
 		if((pwd.charCodeAt(j) < 48 || pwd.charCodeAt(j) > 57) && 
 				(pwd.charCodeAt(j) < 65 || pwd.charCodeAt(j) > 90) && 
 				(pwd.charCodeAt(j) < 97 || pwd.charCodeAt(j) > 122)) {
-
 			pwd_check = false;
 			break;
 		}
 	}
 
+	pwd_check2 = true;
 
-	// SI PAS TROUVE, ON AJOUTE LE NOUVEAU COMPTE DANS LA LISTE DES COMPTES
-
-	if(trouve === false && pwd_len === true && pwd_check === true) {
-		var crea_pw = true;
-	} else {
-	 crea_pw = false;
+	for (j = 0; j < pwd.length; j++){
+		if(query.password[j] !== query.check[j]) {
+			pwd_check2 = false;
+		}
 	}
+			// SI PAS TROUVE, ON AJOUTE LE NOUVEAU COMPTE DANS LA LISTE DES COMPTES
 
-	if (pseudo_len === true && pseudo_check === true) {
-		var crea_pseudo = true;
-	} else {
-		crea_pseudo = false;
-	}
+			if(trouve === false && pwd_len === true && pwd_check === true && pwd_check2 === true) {
+				var crea_pw = true;
+			} else {
+				crea_pw = false;
+			}
 
-	if (crea_pw === true && crea_pseudo === true) { 
-		nouveauMembre = {};
-		nouveauMembre.pseudo = query.pseudo;
-		nouveauMembre.password = query.password;
-		nouveauMembre.statut = "";
-		listeMembres[listeMembres.length] = nouveauMembre;
+			if (pseudo_len === true && pseudo_check === true) {
+				var crea_pseudo = true;
+			} else {
+				crea_pseudo = false;
+			}
 
-		contenu_fichier = JSON.stringify(listeMembres);
+			if (crea_pw === true && crea_pseudo === true) { 
+				nouveauMembre = {};
+				nouveauMembre.pseudo = query.pseudo;
+				nouveauMembre.password = query.password;
+				nouveauMembre.statut = "";
+				listeMembres[listeMembres.length] = nouveauMembre;
 
-		fs.writeFileSync("membres.json", contenu_fichier, 'utf-8');
-	}
+				contenu_fichier = JSON.stringify(listeMembres);
+
+				fs.writeFileSync("membres.json", contenu_fichier, 'utf-8');
+			}
 
 
-	// ON RENVOIT UNE PAGE HTML 
+			// ON RENVOIT UNE PAGE HTML 
 
-	if(trouve === true) {
-		// SI CREATION PAS OK, ON REAFFICHE PAGE FORMULAIRE AVEC ERREUR
+			if(trouve === true) {
+				// SI CREATION PAS OK, ON REAFFICHE PAGE FORMULAIRE AVEC ERREUR
 
-		page = fs.readFileSync('res_inscription.html', 'utf-8');
+				page = fs.readFileSync('res_inscription.html', 'utf-8');
 
-		marqueurs = {};
-		marqueurs.erreur = "ERREUR : ce compte existe déjà";
-		marqueurs.pseudo = query.pseudo;
-		page = page.supplant(marqueurs);
+				marqueurs = {};
+				marqueurs.erreur = "ERREUR : ce compte existe déjà";
+				marqueurs.pseudo = query.pseudo;
+				page = page.supplant(marqueurs);
 
-	} else if(pwd_len === false) {
+			} else if(pwd_len === false) {
 
-		page = fs.readFileSync('res_inscription.html', 'utf-8');
+				page = fs.readFileSync('res_inscription.html', 'utf-8');
 
-		marqueurs = {};
-		marqueurs.erreur = "ERREUR : Veuillez entrer un mot de passe d'au moins 6 caractères";
-		marqueurs.pseudo = query.pseudo;
-		marqueurs.password = query.password;
-		page = page.supplant(marqueurs);
+				marqueurs = {};
+				marqueurs.erreur = "ERREUR : Veuillez entrer un mot de passe d'au moins 6 caractères";
+				marqueurs.pseudo = query.pseudo;
+				marqueurs.password = query.password;
+				page = page.supplant(marqueurs);
 
-	} else if(pwd_check === false) {
+			} else if(pwd_check === false) {
 
-		page = fs.readFileSync('res_inscription.html', 'utf-8');
+				page = fs.readFileSync('res_inscription.html', 'utf-8');
 
-		marqueurs = {};
-		marqueurs.erreur = "ERREUR : Veuillez entrer un mot de passe avec des caractères alphanumérique";
-		marqueurs.pseudo = query.pseudo;
-		marqueurs.password = query.password;
-		page = page.supplant(marqueurs);
+				marqueurs = {};
+				marqueurs.erreur = "ERREUR : Veuillez entrer un mot de passe avec des caractères alphanumérique";
+				marqueurs.pseudo = query.pseudo;
+				marqueurs.password = query.password;
+				page = page.supplant(marqueurs);
 
-	} else if (crea_pseudo === false) {
-		
-		page = fs.readFileSync('res_inscription.html', 'UTF-8');
+			} else if (crea_pseudo === false) {
 
-		marqueurs = {};
-		marqueurs.erreur = "Erreur: Le login est trop long, ou comporte des caractères non autorisés."
-		marqueurs.pseudo = query.pseudo;
-		marqueurs.password = query.password;
-		page = page.supplant(marqueurs);
-	} else {
-		// SI CREATION OK, ON ENVOIE PAGE DE CONFIRMATION
+				page = fs.readFileSync('res_inscription.html', 'UTF-8');
 
-		page = fs.readFileSync('res_confirmation_inscription.html', 'UTF-8');
+				marqueurs = {};
+				marqueurs.erreur = "Erreur: Le login est trop long, ou comporte des caractères non autorisés."
+					marqueurs.pseudo = query.pseudo;
+				marqueurs.password = query.password;
+				page = page.supplant(marqueurs);
 
-		marqueurs = {};
-		marqueurs.pseudo = query.pseudo;
-		marqueurs.password = query.password;
-		page = page.supplant(marqueurs);
-	}
+			} else if (pwd_check2 === false) {
 
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write(page);
-	res.end();
-};
+				page = fs.readFileSync('res_inscription.html','UTF-8');
 
-//---------------------------------------------------------------------------
+				marqueurs = {};
+				marqueurs.erreur = 'Erreur: Les mots de passe ne correspondent pas.';
+				marqueurs.pseudo = query.pseudo;
+				marqueurs.password = query.password;
+				page = page.supplant(marqueurs);
 
-module.exports = trait;
+			} else {
+				// SI CREATION OK, ON ENVOIE PAGE DE CONFIRMATION
+
+				page = fs.readFileSync('res_confirmation_inscription.html', 'UTF-8');
+
+				marqueurs = {};
+				marqueurs.pseudo = query.pseudo;
+				marqueurs.password = query.password;
+				page = page.supplant(marqueurs);
+			}
+
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.write(page);
+			res.end();
+		};
+
+		//---------------------------------------------------------------------------
+
+		module.exports = trait;
