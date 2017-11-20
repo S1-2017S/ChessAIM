ced34f400e5922a94c668d6ba9207dd2e7277f34
FUMEY-HUMBERT <FUMEY-HUMBERT@intechS1.fr>
2017-11-20 13:38:47 +0100

    req_choix

---
+++ req_choix.js
@@ -0,0 +1,29 @@
+//========================================================
+// Traitement de "req_choix"
+// Auteur : Bennaceur / Fumey-humbert / Mercier-Handisyde / Vasseur
+// Version : 20.11.2017
+//========================================================
+
+"use strict";
+
+var fs = require("fs");
+require('remedial');
+
+var trait = function(req, res, query) {
+
+	var page;
+	var marqueurs;
+
+	page = fs.readFileSync('res_placement.html', 'utf-8');
+
+	marqueurs = {};
+	marqueurs.pseudo = query.pseudo;
+	page = page.supplant(marqueurs);
+
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(page);
+	res.end();
+
+};
+
+module.exports = trait;
