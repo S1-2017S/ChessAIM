45587832fe0cee5beaceadb6c5831d58fcef4df3
FUMEY-HUMBERT <FUMEY-HUMBERT@intechS1.fr>
2017-12-08 10:45:46 +0100

    affichage

--- req_choix.js
+++ req_choix.js
@@ -13,6 +13,20 @@ var trait = function(req, res, query) {
 
 	var page;
 	var marqueurs;
+	var marqueurs_board;
+	var contenu_board;
+	var liste_board;
+
+	contenu_board = fs.readFileSync(query.pseudo + ".json", 'utf-8');
+	liste_board = JSON.parse(contenu_board);
+
+	marqueurs_board = {};
+	for(var ligne = 0; ligne < 8; ligne++) {
+		for(var colonne = 0; colonne < 8; colonne++) {
+			marqueurs_board["sqr_" + ligne + ":" + colonne] = liste_board[ligne][colonne];
+		}
+	}
+	page = page.supplant(marqueurs_board);
 
 	page = fs.readFileSync('res_placement.html', 'utf-8');
 
--- req_placement.js
+++ req_placement.js
@@ -13,13 +13,18 @@ var trait = function(req, res, query) {
 
 	var page;
 	var marqueurs;
+	var marqueurs_board;
 	var liste_membre;
+	var liste_board;
 	var contenu_fichier;
+	var contenu_board;
 	var i;
 	var adv;
 
 	contenu_fichier = fs.readFileSync("salon.json", 'utf-8');
+	contenu_board = fs.readFileSync(query.pseudo + ".json", 'utf-8');
 	liste_membre = JSON.parse(contenu_fichier);
+	liste_board = JSON.parse(contenu_board);
 
 	for(i = 0; i < liste_membre.length; i++){
 		if(liste_membre[i].pseudo === query.pseudo){
@@ -33,6 +38,14 @@ var trait = function(req, res, query) {
 			liste_membre[i].statut = "actif";
 		}
 	}
+
+	marqueurs_board = {};
+	for(var ligne = 0; ligne < 8; ligne++) {
+		for(var colonne = 0; colonne < 8; colonne++) {
+			marqueurs_board["sqr_" + ligne + ":" + colonne] = liste_board[ligne][colonne];
+		}
+	}
+	page = page.supplant(marqueurs_board);
 	
 	contenu_fichier = JSON.stringify(liste_membre);
 	fs.writeFileSync("salon.json", contenu_fichier,'utf-8');
