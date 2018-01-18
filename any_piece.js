77ba3cb2d7de099f51c689958572c557abf1e832
BENNACEUR <BENNACEUR@intechS1.fr>
2018-01-18 22:19:53 +0100

    défaite + déplacement autres pièces

---
+++ any_piece.js
@@ -0,0 +1,123 @@
+//========================================================
+// Module déplacement pièces
+// Auteur : Bennaceur / Fumey-humbert / Mercier-Handisyde / Vasseur
+// Version : 20.11.2017
+//========================================================
+
+"use strict";
+
+var fs = require("fs");
+require('remedial');
+
+var any_piece = function(req, res, query) {
+
+	var page;
+	var adv;
+	var pawn;
+	var color;
+	var marqueurs;
+	var marqueurs_board;
+	var contenu_board;
+	var contenu_fichier;
+	var contenu_image;
+	var liste_membre;
+	var liste_board;
+	var liste_image;
+
+
+	page = fs.readFileSync('res_placement.html', 'utf-8');
+
+	contenu_fichier = fs.readFileSync("salon.json", 'utf-8');
+	liste_membre = JSON.parse(contenu_fichier);
+
+	contenu_image = fs.readFileSync("pieces.json", 'utf-8');
+	liste_image = JSON.parse(contenu_image);
+
+	marqueurs = {};
+	marqueurs.pseudo = query.pseudo;
+	for(var i = 0; i < liste_membre.length; i++){
+		if(liste_membre[i].pseudo === query.pseudo){
+
+			adv = liste_membre[i].adv;
+
+			if(liste_membre[i].color === "blanc") {
+
+				color = "blanc";
+				contenu_board = fs.readFileSync(adv + ".json", 'utf-8');
+
+			} else if(liste_membre[i].color === "noir") {
+
+				color = "noir";
+				contenu_board = fs.readFileSync(query.pseudo + ".json", 'utf-8');
+
+			}
+
+			liste_board = JSON.parse(contenu_board);
+		}
+	}
+
+	var l = 7;
+
+	marqueurs_board = {};
+	for(var ligne = 0; ligne < 8; ligne++) {
+
+		var c = 7;
+
+		for(var colonne = 0; colonne < 8; colonne++) {
+
+			pawn = liste_board[ligne][colonne];
+
+			if(color === "noir") {
+
+				if(liste_board[ligne][colonne] === liste_board[ligne][colonne].toUpperCase() && liste_board[ligne][colonne] !== " ") {
+
+					marqueurs_board["sqr_" + l + ":" + c] = 
+						"<img src="+liste_image[pawn]+">";
+
+				} else if(liste_board[ligne][colonne] === liste_board[ligne][colonne].toLowerCase() && liste_board[ligne][colonne] !== " ") {
+
+					marqueurs_board["sqr_" + l + ":" + c] = 
+						"<a href='req_placement?pseudo="+query.pseudo+"&x="+query.x+"&y="+query.y+"&x_new="+ligne+"&y_new="+colonne+"'><img src="+liste_image[pawn]+"></a>";
+
+				} else if(liste_board[ligne][colonne] === " ") {
+
+					marqueurs_board["sqr_" + l + ":" + c] = 
+						"<a href='req_placement?pseudo="+query.pseudo+"&x="+query.x+"&y="+query.y+"&x_new="+ligne+"&y_new="+colonne+"'><img src="+liste_image[pawn]+"></a>";
+
+				}
+
+			} else if (color === "blanc") {
+
+				if(liste_board[ligne][colonne] === liste_board[ligne][colonne].toUpperCase() && liste_board[ligne][colonne] !== " ") {
+
+					marqueurs_board["sqr_" + ligne + ":" + colonne] = 
+						"<a href='req_placement?pseudo="+query.pseudo+"&x="+query.x+"&y="+query.y+"&x_new="+ligne+"&y_new="+colonne+"'><img src="+liste_image[pawn]+"></a>";
+
+				} else if(liste_board[ligne][colonne] === liste_board[ligne][colonne].toLowerCase() && liste_board[ligne][colonne] !== " ") {
+
+					marqueurs_board["sqr_" + ligne + ":" + colonne] = 
+						"<img src="+liste_image[pawn]+">";
+
+				} else if(liste_board[ligne][colonne] === " ") {
+
+					marqueurs_board["sqr_" + ligne + ":" + colonne] = 
+						"<a href='req_placement?pseudo="+query.pseudo+"&x="+query.x+"&y="+query.y+"&x_new="+ligne+"&y_new="+colonne+"'><img src="+liste_image[pawn]+"></a>";
+
+				}
+
+			}
+			c--;
+		}
+		l--;
+	}
+	page = page.supplant(marqueurs_board);
+
+	page = page.supplant(marqueurs);
+
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(page);
+	res.end();
+
+};
+
+module.exports = any_piece;
--- req_placement.js
+++ req_placement.js
@@ -34,7 +34,7 @@ var trait = function(req, res, query) {
 	var c;
 
 	//========== Récupèration des informations de partie =============\\
-	
+
 	contenu_init = fs.readFileSync("init_board.json","UTF-8");
 	liste_init = JSON.parse(contenu_init);
 
@@ -46,22 +46,22 @@ var trait = function(req, res, query) {
 
 	marqueurs = {};
 	marqueurs.pseudo = query.pseudo;
-	
+
 	for(i = 0; i < liste_membre.length; i++){
-		
+
 		if(liste_membre[i].pseudo === query.pseudo){
-			
+
 			adv = liste_membre[i].adv;
-			
+
 			if(liste_membre[i].color === "noir") {
-				
+
 				color = "noir";
 				board = query.pseudo;
 				contenu_board = fs.readFileSync(board + ".json", 'utf-8');
 				liste_board = JSON.parse(contenu_board);
-			
+
 			} else if(liste_membre[i].color === "blanc") {
-				
+
 				color = "blanc";
 				board = adv;
 				contenu_board = fs.readFileSync(board + ".json", 'utf-8');
@@ -74,25 +74,25 @@ var trait = function(req, res, query) {
 	i = Number(query.x_new);
 	l = Number(query.x_new);
 	c = Number(query.y_new);
-//Traitement des différents cas avec modification des informations de partie\\
+	//Traitement des différents cas avec modification des informations de partie\\
 	//=================== Roques ==============\\
 	if(liste_board[query.x][query.y] === "r" && liste_board[query.x_new][query.y_new] === "k") {
-		
+
 		if(query.y === 7) {
-		
-		tmp = liste_board[query.x_new][query.y_new];
-		liste_board[l][c+2] = liste_board[query.x][query.y];
-		liste_board[query.x][query.y] = " ";
-		liste_board[query.x_new][query.y] = " ";
-		liste_board[l][c-2] = tmp;
-		contenu_board = JSON.stringify(liste_board);
-		fs.writeFileSync(board+".json", contenu_board, "UTF-8");
-		
+
+			tmp = liste_board[query.x_new][query.y_new];
+			liste_board[l][c+2] = liste_board[query.x][query.y];
+			liste_board[query.x][query.y] = " ";
+			liste_board[query.x_new][query.y] = " ";
+			liste_board[l][c-2] = tmp;
+			contenu_board = JSON.stringify(liste_board);
+			fs.writeFileSync(board+".json", contenu_board, "UTF-8");
+
 		} else if (query.y === 7) {
 
-	
+
 		}
-	
+
 	} else if(liste_board[query.x][query.y] === "R" && liste_board[query.x_new][query.y_new] === "K") {
 
 		tmp = liste_board[query.x_new][query.y_new];
@@ -100,11 +100,11 @@ var trait = function(req, res, query) {
 		liste_board[query.x][query.y] = tmp;
 		contenu_board = JSON.stringify(liste_board);
 		fs.writeFileSync(board+ ".json", contenu_board, "utf-8");
-		
+
 		//============== Promotions ===============\\
-	
+
 	} else if(liste_board[query.x][query.y] === "p" && i === 0) {
-		
+
 		tmp = liste_board[query.x_new][query.y_new];
 		liste_board[query.x_new][query.y_new] = liste_board[query.x][query.y];
 		liste_board[query.x][query.y] = " ";
@@ -133,7 +133,7 @@ var trait = function(req, res, query) {
 		res.end();
 
 	}else if (liste_board[query.x][query.y] === "P" && i === 7) {
-		
+
 		tmp = liste_board[query.x_new][query.y_new];
 		liste_board[query.x_new][query.y_new] = liste_board[query.x][query.y];
 		liste_board[query.x][query.y] = " ";
@@ -161,10 +161,87 @@ var trait = function(req, res, query) {
 		res.write(page);
 		res.end();
 
-	//================= Prise ou déplacement classique ==================\\
-	
-	} else { 
+
+
+	}else if (liste_board[query.x][query.y] === liste_board[query.x][query.y].toLowerCase() && liste_board[query.x_new][query.y_new] === "K") {
+
+		console.log(liste_board[query.x][query.y]);
+		//========= Modification des status de jeu =============\\
+
+		for(i = 0; i < liste_membre.length; i++){
+			if(liste_membre[i].pseudo === query.pseudo){
+				adv = liste_membre[i].adv;
+				liste_membre[i].statut = "";
+				liste_membre[i].adv = "";
+				liste_membre[i].color = "";
+			}
+
+		}
+
+		for(i = 0; i < liste_membre.length; i++){
+			if(liste_membre[i].pseudo === adv){
+				liste_membre[i].statut = "";
+				liste_membre[i].adv = "";
+				liste_membre[i].color = "";
+			}
+		}
+
+		contenu_fichier = JSON.stringify(liste_membre);
+		fs.writeFileSync("salon.json", contenu_fichier, 'utf-8');
+
+
+		page = fs.readFileSync('res_fin.html','UTF-8');
+		fs.unlinkSync(board +".json");
+		marqueurs = {};
+		marqueurs.pseudo = query.pseudo;
+		marqueurs.vainqueur = query.pseudo;
+		page = page.supplant(marqueurs);
+
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(page);
+		res.end();
+
+	} else if (liste_board[query.x][query.y] === liste_board[query.x][query.y].toUpperCase() && liste_board[query.x_new][query.y_new] === "k") {
 		
+		fs.unlinkSync(board +".json");
+
+		//========= Modification des status de jeu =============\\
+
+		for(i = 0; i < liste_membre.length; i++){
+			if(liste_membre[i].pseudo === query.pseudo){
+				adv = liste_membre[i].adv;
+				liste_membre[i].statut = "";
+				liste_membre[i].adv = "";
+				liste_membre[i].color = "";
+			}
+
+		}
+
+		for(i = 0; i < liste_membre.length; i++){
+			if(liste_membre[i].pseudo === adv){
+				liste_membre[i].statut = "";
+				liste_membre[i].adv = "";
+				liste_membre[i].color = "";
+			}
+		}
+
+		contenu_fichier = JSON.stringify(liste_membre);
+		fs.writeFileSync("salon.json", contenu_fichier, 'utf-8');
+
+
+
+		page = fs.readFileSync('res_fin.html', 'UTF-8');
+		marqueurs = {};
+		marqueurs.pseudo = query.pseudo;
+		marqueurs.vaiqueur = query.pseudo;
+		page = page.supplant(marqueurs);
+
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(page);
+		res.end();
+
+	} else { 
+
 		tmp = liste_board[query.x][query.y];
 		liste_board[query.x][query.y] = " ";
 		liste_board[query.x_new][query.y_new] = tmp;
@@ -173,39 +250,39 @@ var trait = function(req, res, query) {
 		contenu_board = fs.readFileSync(board+".json","UTF-8");
 		liste_board = JSON.parse(contenu_board);
 
-	//============== Changement des status de jeu =================\\
+		//============== Changement des status de jeu =================\\
 
 		for(i = 0; i < liste_membre.length; i++){
-			
+
 			if(liste_membre[i].pseudo === query.pseudo){
-				
+
 				adv = liste_membre[i].adv;	
 				liste_membre[i].statut = "passif"
-			
+
 			}
 		}
 
 		for(i = 0; i < liste_membre.length; i++){
-			
+
 			if(liste_membre[i].pseudo === adv){
 				liste_membre[i].statut = "actif";
-			
+
 			}
 		}
 
 		contenu_fichier = JSON.stringify(liste_membre);
 		fs.writeFileSync("salon.json", contenu_fichier, "UTF-8");
 
-	//=============== Passage en passif/génération plateau ==============\\
+		//=============== Passage en passif/génération plateau ==============\\
 		page = fs.readFileSync("res_passif.html","UTF-8");
 
 		marqueurs_board = {};
 		var l = 7;
-		
+
 		for(var ligne = 0; ligne < 8; ligne++) {
-			
+
 			var c = 7;
-			
+
 			for(var colonne = 0; colonne < 8; colonne++) {
 
 				pawn = liste_board[ligne][colonne];	
